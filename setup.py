fa440158df82e692e1fb49e2e85000962b8fec47
Matthias Kuhn <matthias@opengis.ch>
2017-10-18 09:28:42 +0200

    Add pypi setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'pum',
+    packages = ['pum'],
+    version = '0.3',
+    description = 'Postgres upgrade manager',
+    author = 'Mario Baranzini',
+    author_email = 'mario@opengis.ch',
+    url = 'https://github.com/opengisch/pum',
+    download_url = 'https://github.com/opengisch/pum/archive/0.3.tar.gz', # I'll explain this in a second
+    keywords = ['postgres', 'migration', 'upgrade'],
+    classifiers = [],
+)
