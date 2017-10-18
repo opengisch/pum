7732c8fbed29c0a1e3b6987ed912157f929e68c6
Matthias Kuhn <matthias@opengis.ch>
2017-10-18 09:57:21 +0200

    Update setup.py according to git tag

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'pum',
     packages = ['pum'],
-    version = '0.3',
+    version = '[VERSION]',
     description = 'Postgres upgrade manager',
     author = 'Mario Baranzini',
     author_email = 'mario@opengis.ch',
     url = 'https://github.com/opengisch/pum',
-    download_url = 'https://github.com/opengisch/pum/archive/0.3.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/opengisch/pum/archive/[VERSION].tar.gz', # I'll explain this in a second
     keywords = ['postgres', 'migration', 'upgrade'],
     classifiers = [],
 )
