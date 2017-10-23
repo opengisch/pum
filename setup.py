f77ef1d15297f6b907d2309ad7c423356600943e
Matthias Kuhn <matthias@opengis.ch>
2017-10-23 12:01:39 +0200

    Add yaml dependency

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     download_url = 'https://github.com/opengisch/pum/archive/[VERSION].tar.gz', # I'll explain this in a second
     keywords = ['postgres', 'migration', 'upgrade'],
     classifiers = [],
-    install_requires=['psycopg2 (>=2.7.3)'],
+    install_requires=['psycopg2 (>=2.7.3)', 'yaml'],
 )
