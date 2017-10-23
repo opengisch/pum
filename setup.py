5a9bd216ec49586e26cb3dc76389a71e70e36e4a
Matthias Kuhn <matthias@opengis.ch>
2017-10-23 12:13:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,21 @@ setup(
     author_email = 'mario@opengis.ch',
     url = 'https://github.com/opengisch/pum',
     download_url = 'https://github.com/opengisch/pum/archive/[VERSION].tar.gz', # I'll explain this in a second
-    keywords = ['postgres', 'migration', 'upgrade'],
-    classifiers = [],
-    install_requires=['psycopg2 (>=2.7.3)', 'yaml'],
+    keywords = [
+        'postgres',
+        'migration',
+        'upgrade'
+    ],
+    classifiers = [
+        'Topic :: Database',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: Version Control',
+        'Development Status :: 5 - Production/Stable'
+    ],
+    install_requires = [
+        'psycopg2>=2.7.3',
+        'PyYAML'
+    ],
 )
