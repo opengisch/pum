0e646fe43d0ad89c0502da84cea9b3b279f53bb4
Matthias Kuhn <matthias@opengis.ch>
2017-11-24 11:27:50 +0100

    Automate version printing from git tag

--- .deploy/deploy_to_pypi.sh
+++ .deploy/deploy_to_pypi.sh
@@ -1,4 +1,7 @@
 cp .deploy/pypirc_template ~/.pypirc
+
+sed -i scripts/pum -e "s/\[DEV\]/${TRAVIS_TAG}/g"
+
 sed -i ~/.pypirc -e "s/\[PYPI_USER\]/${PYPI_USER}/g"
 sed -i ~/.pypirc -e "s/\[PYPI_PASSWORD\]/${PYPI_PASSWORD}/g"
 sed -i setup.py -e "s/\[VERSION\]/${TRAVIS_TAG}/g"
