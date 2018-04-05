807a3fbcaa45ed3a5b1f421d1724c8e68b748e58
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-04-05 11:17:25 -0400

    Update dumper.py

--- pum/core/dumper.py
+++ pum/core/dumper.py
@@ -41,7 +41,8 @@ class Dumper:
         command = [
             pg_restore_exe,
             'service={}'.format(self.pg_service),
-            '--exit-on-error --no-owner',
+            '--exit-on-error',
+            '--no-owner',
             self.file]
 
         subprocess.check_output(command, stderr=subprocess.STDOUT)
