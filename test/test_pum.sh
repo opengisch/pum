5d4bf3c218cc1050cadd7f6f13880e2311c259d5
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-04-07 21:38:16 -0400

    fix deploy again

---
+++ test/test_pum.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+dropdb pum_test_1 && dropdb pum_test_2 && createdb pum_test_1 && createdb pum_test_2
+dropdb pum_test_3 && createdb pum_test_3
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+${DIR}/../scripts/pum test-and-upgrade \
+  -pp pum_test_1 -pt pum_test_2 -pc pum_test_3 \
+  -t qgep_sys.pum_info \
+  -d delta/ \
+  -f /tmp/qwat_dump \
+  -i constraints views \
+  --skip-schemas public
