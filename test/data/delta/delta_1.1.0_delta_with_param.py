99b50c27932cdff8263a3dcc4c59c11881dd46f6
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-04-24 15:02:30 -0500

    add test for variable in Python deltas

---
+++ test/data/delta/delta_1.1.0_delta_with_param.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import psycopg2
+from pum.core.deltapy import DeltaPy
+
+
+class CreateViews(DeltaPy):
+
+    def run(self):
+        my_field_length = self.variable('my_field_length')
+
+        conn = psycopg2.connect("service={0}".format(self.pg_service))
+        cursor = conn.cursor()
+        sql = "ALTER TABLE northwind.customers ALTER COLUMN country TYPE varchar({l});".format(l=my_field_length)
+        cursor.execute(sql, self.variables)
+        conn.commit()
+        conn.close()
+
+
