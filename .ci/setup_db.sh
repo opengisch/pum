77c18ec35b961bdd0b2f2d80e022e87e3bc743a0
olivierdalang <olivier.dalang@gmail.com>
2020-02-19 15:01:39 +0100

    (to be squashed) try fix setup_db

--- .ci/setup_db.sh
+++ .ci/setup_db.sh
@@ -10,7 +10,7 @@ else
 fi
 
 for pgsrv in pum_test_1 pum_test_2 pum_test_3; do
-  printf "[${pgsrv}]\nhost=localhost\ndbname=${pgsrv}\nuser=postgres\n\n" >> $PGSERVICE_FILE
+  printf "[${pgsrv}]\nhost=localhost\ndbname=${pgsrv}\nuser=postgres\n\n" >> "$PGSERVICE_FILE"
   dropdb --if-exists ${pgsrv}
   createdb ${pgsrv}
 done
