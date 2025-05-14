3defca7cb745dbbf70eced812abcb57a7b9926be
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-05-14 21:58:05 +0200

    remove obsolete code

--- .deploy/deploy_to_pypi.sh
+++
@@ -1,9 +0,0 @@
-cp .deploy/pypirc_template ~/.pypirc
-
-sed -i pum/__main__.py -e "s/\[DEV\]/${GIT_TAG_NAME}/g"
-
-sed -i ~/.pypirc -e "s/\[PYPI_USER\]/${PYPI_USER}/g"
-sed -i ~/.pypirc -e "s/\[PYPI_PASSWORD\]/${PYPI_PASSWORD}/g"
-sed -i setup.py -e "s/\[VERSION\]/${GIT_TAG_NAME}/g"
-
-python setup.py sdist upload -r pypi
