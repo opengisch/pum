1bf2d20dd659c543c84f32c795723c0d139b4ae6
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-06-16 17:02:56 +0200

    fix info + cli

--- pum/info.py
+++ pum/info.py
@@ -3,9 +3,11 @@ import sys
 import psycopg
 
 from .pum_config import PumConfig
-from .schema_migrations import SchemaMigrations
+from .schema_migrations import SchemaMigrations, MIGRATION_TABLE_NAME
 
 logger = logging.getLogger(__name__)
+# set to info here
+logger.setLevel(logging.INFO)
 
 
 def run_info(connection: psycopg.Connection, config: PumConfig) -> None:
@@ -20,11 +22,17 @@ def run_info(connection: psycopg.Connection, config: PumConfig) -> None:
         schema_migrations = SchemaMigrations(config=config)
         if not schema_migrations.exists(connection=connection):
             logger.info(
-                f"No migrations found in {config.pum.migration_table_schema}.{config.pum.migration_table_name}."
+                f"No migrations found in {config.config.pum.migration_table_schema}.{MIGRATION_TABLE_NAME}."
             )
         else:
             # Add your logic for when migrations exist; for now, we simply print a message.
             logger.info("Migrations found.")
+            logger.info(
+                f"Schema migrations table: {config.config.pum.migration_table_schema}.{MIGRATION_TABLE_NAME}"
+            )
+            logger.info(
+                f"Version: {schema_migrations.migration_details(connection=connection)['version']}"
+            )
     except Exception:
         logger.exception("An error occurred while checking for migrations.")
         sys.exit(1)
